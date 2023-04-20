d12e2b2c3a9dff1765e4646768977ff921e17750
Yohanes <yohanesmulugeta21@gmail.com>
2023-04-20 10:43:00 +0300

    basics of interfaces.

--- interfaces.ts
+++ interfaces.ts
@@ -1,14 +1,22 @@
 // INTERFACES ARE NEW TYPES WE CREATE
+
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
 };
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken: ${vehicle.broken}`);
+  // console.log(`Has Pussy: ${vehicle.hasPussy}`);
 };
 
 printVehicle(oldCivic);
