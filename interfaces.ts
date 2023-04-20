7a6f5403d0a4ac222d1b9ebeb3ba2bac5a59c136
Yohanes <yohanesmulugeta21@gmail.com>
2023-04-20 12:49:09 +0300

    expirment with diffrent values.

--- interfaces.ts
+++ interfaces.ts
@@ -13,9 +13,20 @@ const oldCivic = {
   },
 };
 
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugarContent: 40,
+  summary() {
+    return `My drink has ${this.sugarContent} grams of sugar`;
+  },
+};
+
 const printSummary = (item: Reportable): void => {
-  // This will only check if the item satsfies the Reportable Interface
+  // This will only check if the item satsfies the Reportable Interface That is
+  // whether the item has a sumary function that returns a string or not
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(drink);
