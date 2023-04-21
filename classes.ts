f428a585f4634a37c0eeec52291e4a69866cf654
Yohanes <yohanesmulugeta21@gmail.com>
2023-04-21 11:51:12 +0300

    experiment with fields and methods inheritance.

--- classes.ts
+++ classes.ts
@@ -1,25 +1,36 @@
 class Vehicle {
-  protected drive(): void {
-    console.log('chagga chugga');
+  protected color: string;
+
+  constructor(color: string) {
+    this.color = color.slice(0, 1).toUpperCase() + color.slice(1).toLowerCase();
   }
 
-  protected honk(): void {
+  protected drive(): void {
+    console.log('chagga chugga');
+    this.honk();
+  }
+
+  private honk(): void {
     console.log('Beep');
   }
 }
 
 class Car extends Vehicle {
+  constructor(color: string, protected brand: string) {
+    super(color);
+  }
+
   protected drive(): void {
     console.log('vroohm vrooooohhm  ');
   }
 
   startDrivingProcess(): void {
     this.drive();
-    this.honk();
+    console.log(`A ${this.color} ${this.brand} Car Started Rolling.`);
   }
 }
 
-const toyota = new Car();
+const toyota = new Car('white', 'Toyota');
 toyota.startDrivingProcess();
 
 // const vehicle = new Vehicle();
